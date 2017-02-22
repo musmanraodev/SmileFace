457ec34dfc2c96296e11deb25ea0ed4eb036a955
Muhammad Usman <musmanraodev@gmail.com>
2017-02-22 13:57:23 -0500

    Add files via upload

---
+++ Drawing 2nd -- Muhammad Usman and Bunmi (Both of us worked and we switched in the middle)/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(800, 800);
+}
+
+function draw() {
+
+  background("pink");
+  ellipse(400, 300, 250, 250);
+
+  ellipse(350, 250, 30, 30);
+  ellipse(450, 250, 30, 30);
+
+  arc(390, 360, 70, 70, 0, PI + QUARTER_PI, CHORD);
+
+
+
+
+}
+
